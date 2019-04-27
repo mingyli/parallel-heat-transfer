fde627aafd79534efdbb6409d371eb68b0ab772f
jonmurata <jmurata@berkeley.edu>
2019-04-27 14:50:58 -0700

    initial 1D openmp framework

--- openmp.cpp
+++ openmp.cpp
@@ -3,94 +3,92 @@
 #include <assert.h>
 #include <math.h>
 #include "common.h"
+#include "omp.h"
 
 //
 //  benchmarking program
 //
 int main( int argc, char **argv )
-{    
+{   
 
     if( find_option( argc, argv, "-h" ) >= 0 )
     {
         printf( "Options:\n" );
         printf( "-h to see this help\n" );
-        printf( "-n <int> to set the number of particles\n" );
+        printf( "-n <int> to set number of particles\n" );
         printf( "-o <filename> to specify the output file name\n" );
-        printf( "-s <filename> to specify a summary file name\n" );
-        printf( "-no turns off all correctness checks and particle output\n");
+        printf( "-s <filename> to specify a summary file name\n" ); 
+        printf( "-no turns off all correctness checks and particle output\n");   
         return 0;
     }
-    
-    int n = read_int( argc, argv, "-n", 1000 );
 
+    int n = read_int( argc, argv, "-n", 1000 );
     char *savename = read_string( argc, argv, "-o", NULL );
     char *sumname = read_string( argc, argv, "-s", NULL );
-    
+
     FILE *fsave = savename ? fopen( savename, "w" ) : NULL;
-    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;
+    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;      
 
     node_t *tnodes = (node_t *) malloc( n * sizeof(node_t) );
     set_len( n );
     init_bar( tnodes, 400, 200 );
 
-    if( find_option( argc, argv, "-no" ) == -1 )
-        {
-          //
-          //  save if necessary
-          //
-          if( fsave )
-              save( fsave, 0, n, tnodes );
-        }
-    
     //
     //  simulate a number of time steps
     //
     double simulation_time = read_timer( );
-	
+
+    #pragma omp parallel
+    {
+    numthreads = omp_get_num_threads();
     for( int step = 0; step < NSTEPS; step++ )
     {
         //
         //  sum temperatures for approximation
         //
-        // We know the ends are fixed in 1D
-        // no checks required.
+        #pragma omp for
         for( int i = 1; i < n-1; i++ )
         {
-		  apply_tsum( tnodes[i], tnodes[i-1]);
+          apply_tsum( tnodes[i], tnodes[i-1]);
           apply_tsum( tnodes[i], tnodes[i+1]);
         }
- 
+        
+		
         //
         //  move particles
         //
+        #pragma omp for
         for( int i = 0; i < n; i++ ) 
-          tupdate( tnodes[i], 1);		
-
-        if( find_option( argc, argv, "-no" ) == -1 )
+            move( particles[i] );
+  
+        if( find_option( argc, argv, "-no" ) == -1 ) 
         {
           //
           //  save if necessary
           //
+          #pragma omp master
           if( fsave && (step%SAVEFREQ) == 0 )
-            save( fsave, step, n, tnodes );
+              save( fsave, n, particles );
         }
     }
+}
     simulation_time = read_timer( ) - simulation_time;
     
-    printf( "n = %d, simulation time = %g seconds\n", n, simulation_time);
+    printf( "n = %d,threads = %d, simulation time = %g seconds", n,numthreads, simulation_time);
 
     //
     // Printing summary data
     //
-    // if( fsum) 
-    //    fprintf(fsum,"%d %g\n",n,simulation_time);
- 
+    // if( fsum)
+    //     fprintf(fsum,"%d %d %g\n",n,numthreads,simulation_time);
+
     //
     // Clearing space
     //
     if( fsum )
-        fclose( fsum );    
-    free( tnodes );
+        fclose( fsum );
+
+    free( particles );
     if( fsave )
         fclose( fsave );
     
